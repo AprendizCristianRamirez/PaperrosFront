a5e146af846307a67733c3dca4399123d88f7e8c
Emanuel Gómez Díaz <egomez413@misena.edu.co>
2023-06-26 17:35:27 -0500

    proceso de creacion de admindash
    no sirve

--- app/app.js
+++ app/app.js
@@ -5,14 +5,17 @@ import ejs from "ejs";
 import path from "path";
 import { fileURLToPath } from 'url';
 import { loginRouter } from "./routes/login.routes.js";
+import { loginRouterAdmin } from "./routes/loginAdmin.routes.js";
 import passport from "passport";
 import cookieparser from "cookie-parser";
 import bodyParser from "body-parser";
 import "./config/middlewares/google.js"
 //RUTAS
 import dashPaseador from "./routes/dashPaseador.routes.js";
+import dash from "./routes/dashAdmin.routes.js";
 import dashDueno from "./routes/dashDueno.routes.js";
 import home from "./routes/homepage.routes.js";
+import dashAdmin from "./routes/dashAdmin.routes.js";
 
 
 //INICIALIZACION
@@ -35,10 +38,19 @@ app.use(cookieparser());
 
 //RUTAS
 app.use("/v1/dueno", dashDueno);
+app.use("/v1/admin", dashAdmin);
 app.use("/v1/paseador", dashPaseador);
 app.get("/", (req, res)=>{
     res.render("home");
 })
+
+app.use("/auth/admin", passport.authenticate("auth-google", {
+    scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email"
+    ],
+    session: false,
+}), loginRouterAdmin);
 
 app.use("/auth", passport.authenticate("auth-google", {
     scope: [
