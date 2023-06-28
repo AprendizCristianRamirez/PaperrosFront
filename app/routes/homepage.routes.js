70c66471d366085217053cda1cd43551d6a9d986
Emanuel Gómez Díaz <egomez413@misena.edu.co>
2023-06-28 17:04:33 -0500

    b

--- app/routes/homepage.routes.js
+++ app/routes/homepage.routes.js
@@ -5,39 +5,39 @@ const home = Router();
 home.get("/", (req, res)=>{
     res.render("homeViews/Inicio.ejs", {
         "titulo":"Paperros", "activo":1,
-        "google": proccess.env.GOOGLE_LOGIN
+        "google": process.env.GOOGLE_LOGIN
     });
 });
 home.get("/Acerca", (req, res)=>{
     res.render("homeViews/Acerca", {
         "titulo":"Paperros","activo":2,
-        "google": proccess.env.GOOGLE_LOGIN
+        "google": process.env.GOOGLE_LOGIN
     });
 });
 home.get("/Servicios", (req, res)=>{
     res.render("homeViews/servicios", {
         "titulo":"Paperros","activo":3,
-        "google": proccess.env.GOOGLE_LOGIN
+        "google": process.env.GOOGLE_LOGIN
     });
 });
 home.get("/Paseadores", (req, res)=>{
     res.render("homeViews/paseadores", {
         "titulo":"Paperros","activo":4,
-        "google": proccess.env.GOOGLE_LOGIN
+        "google": process.env.GOOGLE_LOGIN
     });
 });
 home.get("/Contactanos", (req, res)=>{
     res.render("homeViews/contactanos", {
         "titulo":"Paperros","activo":5,
-        "google": proccess.env.GOOGLE_LOGIN,
-        "api": proccess.env.API
+        "google": process.env.GOOGLE_LOGIN,
+        "api": process.env.API
         });
 });
 
 home.get("/Registro", (req, res)=>{
     res.render("homeViews/registro", {
         "titulo":"Paperros","activo":7,
-        "google": proccess.env.GOOGLE_LOGIN
+        "google": process.env.GOOGLE_LOGIN
         });
 });
 
--- dist/routes/homepage.routes.js
+++ dist/routes/homepage.routes.js
@@ -10,43 +10,43 @@ home.get("/", function (req, res) {
   res.render("homeViews/Inicio.ejs", {
     "titulo": "Paperros",
     "activo": 1,
-    "google": proccess.env.GOOGLE_LOGIN
+    "google": process.env.GOOGLE_LOGIN
   });
 });
 home.get("/Acerca", function (req, res) {
   res.render("homeViews/Acerca", {
     "titulo": "Paperros",
     "activo": 2,
-    "google": proccess.env.GOOGLE_LOGIN
+    "google": process.env.GOOGLE_LOGIN
   });
 });
 home.get("/Servicios", function (req, res) {
   res.render("homeViews/servicios", {
     "titulo": "Paperros",
     "activo": 3,
-    "google": proccess.env.GOOGLE_LOGIN
+    "google": process.env.GOOGLE_LOGIN
   });
 });
 home.get("/Paseadores", function (req, res) {
   res.render("homeViews/paseadores", {
     "titulo": "Paperros",
     "activo": 4,
-    "google": proccess.env.GOOGLE_LOGIN
+    "google": process.env.GOOGLE_LOGIN
   });
 });
 home.get("/Contactanos", function (req, res) {
   res.render("homeViews/contactanos", {
     "titulo": "Paperros",
     "activo": 5,
-    "google": proccess.env.GOOGLE_LOGIN,
-    "api": proccess.env.API
+    "google": process.env.GOOGLE_LOGIN,
+    "api": process.env.API
   });
 });
 home.get("/Registro", function (req, res) {
   res.render("homeViews/registro", {
     "titulo": "Paperros",
     "activo": 7,
-    "google": proccess.env.GOOGLE_LOGIN
+    "google": process.env.GOOGLE_LOGIN
   });
 });
 var _default = home;
