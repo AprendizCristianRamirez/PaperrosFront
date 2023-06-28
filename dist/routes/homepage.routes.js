d3b8995935c9e7b0f93fc89eb977293547c30acc
Emanuel Gómez Díaz <egomez413@misena.edu.co>
2023-06-28 16:53:13 -0500

    Subida para la presentacion 2
    Subida para la presentacion 2

--- dist/routes/homepage.routes.js
+++ dist/routes/homepage.routes.js
@@ -10,35 +10,35 @@ home.get("/", function (req, res) {
   res.render("homeViews/Inicio.ejs", {
     "titulo": "Paperros",
     "activo": 1,
-    "google": process.env.GOOGLE_LOGIN
+    "google": proccess.env.GOOGLE_LOGIN
   });
 });
 home.get("/Acerca", function (req, res) {
   res.render("homeViews/Acerca", {
     "titulo": "Paperros",
     "activo": 2,
-    "google": process.env.GOOGLE_LOGIN
+    "google": proccess.env.GOOGLE_LOGIN
   });
 });
 home.get("/Servicios", function (req, res) {
   res.render("homeViews/servicios", {
     "titulo": "Paperros",
     "activo": 3,
-    "google": process.env.GOOGLE_LOGIN
+    "google": proccess.env.GOOGLE_LOGIN
   });
 });
 home.get("/Paseadores", function (req, res) {
   res.render("homeViews/paseadores", {
     "titulo": "Paperros",
     "activo": 4,
-    "google": process.env.GOOGLE_LOGIN
+    "google": proccess.env.GOOGLE_LOGIN
   });
 });
 home.get("/Contactanos", function (req, res) {
   res.render("homeViews/contactanos", {
     "titulo": "Paperros",
     "activo": 5,
-    "google": process.env.GOOGLE_LOGIN,
+    "google": proccess.env.GOOGLE_LOGIN,
     "api": proccess.env.API
   });
 });
@@ -46,7 +46,7 @@ home.get("/Registro", function (req, res) {
   res.render("homeViews/registro", {
     "titulo": "Paperros",
     "activo": 7,
-    "google": process.env.GOOGLE_LOGIN
+    "google": proccess.env.GOOGLE_LOGIN
   });
 });
 var _default = home;
